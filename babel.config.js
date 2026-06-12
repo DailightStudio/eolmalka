67059013fa4d6eaf9acf07ca12e8455d790c345b
Jinho <32083859+DailightStudio@users.noreply.github.com>
2026-06-12 11:12:52 +0900

    fix: Babel에서 react-native-worklets 플러그인 제거 (Reanimated 3.x용)

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    // reanimated 4부터는 worklets 플러그인을 별도 패키지에서 가져옴
-    plugins: ["react-native-worklets/plugin"],
   };
 };
